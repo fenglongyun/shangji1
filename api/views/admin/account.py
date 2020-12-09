13b7eaeacdfed95fd0a392a8bd1e3e1e5d6d5106
fly <19121711185@163.com>
2020-12-09 18:59:54 +0800

    '9citijiao'

--- api/views/admin/account.py
+++ api/views/admin/account.py
@@ -14,11 +14,7 @@ class UnauthorizedUsers(APIView):
         queryset=models.UserInfo.objects.filter(category='0').all()
         ser=serializer.UserInfoSerializer(queryset,many=True)
         return Response(ser.data,headers={"Access-Control-Allow-Origin":"*"})
-<<<<<<< HEAD:api/views/admin/account.py
         
-=======
-
->>>>>>> 7f6bd5141e3b4b0485fc35bba04f0ebea7f5096e:api/views/adminviews.py
 class UnauthorizedUser(APIView):
     authentication_classes=[JwtAuth]
     permission_classes=[MyPermission1]
@@ -30,11 +26,7 @@ class UnauthorizedUser(APIView):
             "code" : 3000,
             "msg" : '删除成功',
         })
-<<<<<<< HEAD:api/views/admin/account.py
 
-=======
-           
->>>>>>> 7f6bd5141e3b4b0485fc35bba04f0ebea7f5096e:api/views/adminviews.py
 class AuthorizedUsers(APIView):
     authentication_classes=[JwtAuth]
     permission_classes=[MyPermission1]
@@ -152,12 +144,3 @@ class UserMenus(APIView):
             user_menu_objs.append(models.UserInfo_Menu(userinfo_id=pk,menu_id=x))
         models.UserInfo_Menu.objects.bulk_create(user_menu_objs)
         return Response({'code':1,'msg':'菜单权限修改成功'},headers={"Access-Control-Allow-Origin":"*"})
-
-<<<<<<< HEAD:api/views/admin/account.py
-
-    
-
-    
-=======
-  
->>>>>>> 7f6bd5141e3b4b0485fc35bba04f0ebea7f5096e:api/views/adminviews.py
